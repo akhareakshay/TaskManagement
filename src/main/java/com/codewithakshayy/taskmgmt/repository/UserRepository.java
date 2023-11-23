880172c0bfb907c3479031e3c5ada7f1e0916a7b
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-11-23 08:48:50 +0530

    created repository

---
+++ src/main/java/com/codewithakshayy/taskmgmt/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.codewithakshayy.taskmgmt.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.codewithakshayy.taskmgmt.document.User;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
