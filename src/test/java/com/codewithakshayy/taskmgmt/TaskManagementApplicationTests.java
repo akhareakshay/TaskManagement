f8191c230a21fb56b779277c65169dc19f0216c9
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-11-23 06:56:47 +0530

    initial commit - project structure

---
+++ src/test/java/com/codewithakshayy/taskmgmt/TaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithakshayy.taskmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
