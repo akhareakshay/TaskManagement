e56ff9aa99c0af8e46d6273927680e6d0ea1c2f4
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-11-30 09:45:29 +0530

    changes in exception handler

--- src/main/java/com/codewithakshayy/taskmgmt/exception/GlobalExceptionHandler.java
+++ src/main/java/com/codewithakshayy/taskmgmt/exception/GlobalExceptionHandler.java
@@ -23,4 +23,8 @@ public class GlobalExceptionHandler {
 		return new ErrorResponse(HttpStatus.NO_CONTENT.value(), nullPointerException.getMessage());
 	}
 
+	public @ResponseBody ErrorResponse handleNoRecordsException(NoRecordsException noRecordsException) {
+		return new ErrorResponse(HttpStatus.NO_CONTENT.value(), noRecordsException.getMessage());
+	}
+
 }
